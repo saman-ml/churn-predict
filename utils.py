213d53b93f359a7bd34bcc3924ee03feb09fdc67
saman babaie <samanbabaie403@gmail.com>
2025-01-20 02:07:11 +0330

    edit utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv('IT_customer_churn.csv')
+df = pd.read_csv('data_preprop.csv')
 
-InternetService_list = df['InternetService'].unique()
-Contract_list = df['Contract'].unique()
-PaymentMethod_list = df['PaymentMethod'].unique()
+Contract_list = df.columns[17:20]
+PaymentMethod_list = df.columns[20:24]
+InternetService_list = df.columns[24:]
