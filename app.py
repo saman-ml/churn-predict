86f9cf8957b18b822062afdc64d3455be2282e36
saman babaie <samanbabaie403@gmail.com>
2025-01-20 01:22:55 +0330

    edit app.py

--- app.py
+++ app.py
@@ -47,15 +47,15 @@ if st.button("Predict Price"):
     
     for InternetService_col in InternetService_list:
         user_data[InternetService_col] = 0
-        user_data[InternetService] = 1
+    user_data[InternetService] = 1
         
     for Contract_list_col in Contract_list:
         user_data[Contract_list_col] = 0
-        user_data[Contract] = 1
+    user_data[Contract] = 1
         
     for col in PaymentMethod_list:
         user_data[col] = 0
-        user_data[PaymentMethod] = 1
+    user_data[PaymentMethod] = 1
     
     prediction = model.predict(user_data)[0]
     
