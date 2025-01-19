6945fe961c86bd840318ac5370b34df0ed01e5c3
saman babaie <samanbabaie403@gmail.com>
2025-01-20 02:30:47 +0330

    edit app.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 df = pd.read_csv('data_preprop.csv')    
-Contract_list = df.columns[17:20]
-PaymentMethod_list = df.columns[20:24]
-InternetService_list = df.columns[24:28]
+Contract_list = df.columns[18:21]
+PaymentMethod_list = df.columns[21:25]
+InternetService_list = df.columns[25:28]
